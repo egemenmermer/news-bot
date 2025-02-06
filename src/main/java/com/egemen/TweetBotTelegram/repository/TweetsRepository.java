7d1dbe5878a758a58e9787492f810b46b82a7436
egemenmermer <egemenmermer@gmail.com>
2025-02-06 12:05:47 +0100

    update

--- src/main/java/com/egemen/TweetBotTelegram/repository/TweetsRepository.java
+++ src/main/java/com/egemen/TweetBotTelegram/repository/TweetsRepository.java
@@ -5,7 +5,7 @@ import com.egemen.TweetBotTelegram.enums.TweetStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.param.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.sql.Timestamp;
